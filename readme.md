77c19130ed4e19d4b59eee9ff44102d1b8ef97a4
huangziyi01 <huangziyi01@baidu.com>
2016-08-25 10:42:15 +0800

    quick push

---
+++ readme.md
@@ -0,0 +1,9 @@
+# 盖亚原生基础组件
+
+传入编辑器或者 `Preview` 以拓展基础组件支持
+
+---
+
+````jsx
+npm install fit-gaea-native-components
+````
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../gaea/models/tsd.d.ts" />
+
+import * as React from 'react'
+import Layout from './layout'
+import Paragraph from './paragraph'
+
+export default [Layout, Paragraph] as Array<React.ComponentClass<FitGaea.ComponentProps>>
